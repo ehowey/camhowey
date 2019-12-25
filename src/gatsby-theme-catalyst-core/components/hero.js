7044419b88357ae267bcf0267531f52b59c0b477
Eric Howey <ehowey@gmail.com>
2019-12-25 00:20:19 -0500

    Christmas Updates

--- src/gatsby-theme-catalyst-core/components/hero.js
+++ src/gatsby-theme-catalyst-core/components/hero.js
@@ -1,7 +1,20 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const HeaderHero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      xmas: file(relativePath: { eq: "xmas.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 720) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
   return (
     <section
       sx={{
@@ -13,13 +26,31 @@ const HeaderHero = () => {
         textAlign: "center",
       }}
     >
-      <Styled.h1>Merry Christmas Dad!</Styled.h1>
-      <Styled.p>
-        I figured with all of your work on gratitude, health and giving you
-        needed a better way to share all of that important information with the
-        world! Hopefully we can work together to make this an easy to use place
-        for you to post some of this information for everyone to see!
-      </Styled.p>
+      <div
+        sx={{
+          maxWidth: "maxContentWidth",
+          margin: "0 auto",
+        }}
+      >
+        <Img
+          sx={{
+            height: "300px",
+          }}
+          fluid={data.xmas.childImageSharp.fluid}
+          alt="Christmas Scene"
+        />
+        <Styled.h1>Merry Christmas Dad!</Styled.h1>
+        <Styled.p>
+          I figured with all of your work on gratitude, health and giving you
+          needed a better way to share it with the world!
+        </Styled.p>
+        <Styled.p>
+          I am going to build you a quick and simple blog site you can use to
+          post more of your information and build an audience for what you need
+          to say. We can chat specifics in the new year!
+        </Styled.p>
+        <Styled.p>Love, Eric</Styled.p>
+      </div>
     </section>
   )
 }
