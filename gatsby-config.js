ba7ef54ff1dbe39198d8bf780cd9256a95497cdf
Eric Howey <ehowey@gmail.com>
2020-01-14 22:17:53 -0700

    Updated site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: `Cam Howey`,
-    description: `Under Development`,
-    keywords: [`gratitude`, `writer`, `giving`, `Cam Howey`],
+    description: `Cam Howey is a retired engineer with a passion for applying big data to healthcare, generosity, gratitude, and kindness. He lives in Calgary, Alberta.`,
+    keywords: [
+      `gratitude`,
+      `writer`,
+      `giving`,
+      `generosity`,
+      `wellness`,
+      `data`,
+      `health`,
+      `healthcare`,
+      `volunteering`,
+      `giving`,
+      `Cam Howey`,
+    ],
     author: `Cam Howey`,
     twitter: ``,
     siteUrl: `https://www.camhowey.com`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
